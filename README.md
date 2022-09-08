53a7b5d8f736bc7a63a9a722bda08fceaf647f8a
Lorran Cruz <112294310+lorrancruz@users.noreply.github.com>
2022-09-08 02:03:03 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Calculadora de média #AULA1 #ALURA #EXTRA
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/lorrancruz/pen/mdLJdmO](https://codepen.io/lorrancruz/pen/mdLJdmO).
+
---
+++ dist/script.js
@@ -0,0 +1,16 @@
+var nome = "Lorran"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 10
+var notaDoTerceiroBimestre = 8
+var notaDoQuartoBimestre = 7
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+//REVISÃO AULA 1
+//variaveis, string, console.log, toFixed, operações matematicas, concatenação
